160009a15e2d4911e0900aa2170181e7c3a98d50
Kripa <z5591052@ad.unsw.edu.au>
2026-05-19 17:58:43 +1000

    setting up server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,3 +1,28 @@
+import express, { json, Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
+const app = express();
+app.use(json());
+app.use(cors({
+    origin: [
+        "http://localhost:8080"
+    ],
+    credentials: true
+}));
+app.use(morgan('dev'));
+
+const PORT: number = parseInt(process.env.PORT || "3000");
+const HOST: string = process.env.IP || '127.0.0.1';
+
+//////////////////////////////// Routes ////////////////////////////////////////
+app.get("/", async (req: Request, res: Response) => {
+    console.log("health check");
+    res.status(200).json({
+        message: "server is up!",
+    });
+});
+
+app.listen(3000);
 
 console.log("please work");
